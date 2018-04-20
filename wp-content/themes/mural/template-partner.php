66c20425994796d39da1a23f8341d439ef3259ba
Maxime Moisan <maxime.moisan@codems.ca>
2018-04-20 10:13:56 -0400

    Ajout de plugins pour import et ajout des catégories pour les partenaires.

--- wp-content/themes/mural/template-partner.php
+++ wp-content/themes/mural/template-partner.php
@@ -30,7 +30,7 @@ get_header(); ?>
 					
 					foreach($tiers as $tier){
 						$logo_width = get_field('largeur', 'tier_'.$tier->term_id);
-
+                       echo "<h2>".$tier->name."</h2>";
 						$args = array(
 							'post_type' => array( 'partner' ),
 							'posts_per_page' => '-1',
@@ -51,8 +51,9 @@ get_header(); ?>
 									
 									$website = get_field('site_web');
 									$name = get_the_title();
+                               
 									?>
-					
+					                <h2><?= $tiers->name ?></h2>
 									<figure class="partner">
 										<?php if($website): ?>
 											<a href="<?= $website ?>" title="<?php printf("View %s's website", $name); ?>" target="_blank">
