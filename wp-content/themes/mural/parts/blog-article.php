4cf2d5604dbac15ce073c8a47093895f7b199a75
Vincent Morin <vincent.morin@codems.ca>
2018-04-24 17:33:44 -0400

    Met la page blog full width...

--- wp-content/themes/mural/parts/blog-article.php
+++ wp-content/themes/mural/parts/blog-article.php
@@ -1,17 +1,19 @@
 <div class="article">
-    <figure>
-        <a href="<?php the_permalink(); ?>">
-            <?= wp_get_attachment_image(get_field('image_a_la_une'), 'blog-preview'); ?>
-        </a>
-    </figure>
-    <div class="description">
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <?php if(has_category()): ?>
-            <!--<div class="blog-categories">
-                <?= get_the_category_list( ', ', '', get_the_ID() ); ?>
-            </div>-->
-        <?php endif;  ?>
-        <?= truncate(get_field('resume'), 145, "&hellip;", false); ?>
-        <a class="readmore" href="<?php the_permalink(); ?>"><?php _e('Learn more +', 'site-theme'); ?></a>
+    <div class="content">
+        <figure>
+            <a href="<?php the_permalink(); ?>">
+                <?= wp_get_attachment_image(get_field('image_a_la_une'), 'blog-preview'); ?>
+            </a>
+        </figure>
+        <div class="description">
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php if(has_category()): ?>
+                <!--<div class="blog-categories">
+                    <?= get_the_category_list( ', ', '', get_the_ID() ); ?>
+                </div>-->
+            <?php endif;  ?>
+            <?= truncate(get_field('resume'), 145, "&hellip;", false); ?>
+            <a class="readmore" href="<?php the_permalink(); ?>"><?php _e('Learn more +', 'site-theme'); ?></a>
+        </div>
     </div>
 </div>
