8206fc75383a999acb83eb2b13646f5763db9a03
Olivier Lamarche Cloutier <olivier.lamarche@codems.ca>
2018-04-24 11:52:04 -0400

    oups script dans la balise

--- wp-content/themes/mural/footer.php
+++ wp-content/themes/mural/footer.php
@@ -27,16 +27,16 @@
 		
 	</div><!-- #page -->
 
-	<?php wp_footer(); ?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118017697-1"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'UA-118017697-1');
-	</script>
+	<?php wp_footer(); ?>	
 </div> <!--pour mmenu-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118017697-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-118017697-1');
+</script>
 </body>
 </html>
