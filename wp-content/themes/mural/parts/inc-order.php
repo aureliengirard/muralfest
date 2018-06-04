1dca064a372ae3c7544959c183e5a3d52cdff133
Vincent Morin <vincent.morin@codems.ca>
2018-06-04 11:47:20 -0400

    Retire le filtre par artiste pour la programmation

--- wp-content/themes/mural/parts/inc-order.php
+++ wp-content/themes/mural/parts/inc-order.php
@@ -25,57 +25,6 @@ if (isset($_GET['category'])) {
                 <div></div>
             </div>
 
-            <?php
-            $args = array(
-                'post_type' => array( 'artist' ),
-                'posts_per_page' => '-1',
-                'order' => 'ASC',
-                'order_by' => 'title',       
-
-            );
-            $query = new WP_Query( $args );
-
-                if ( $query->have_posts() ) : ?>
-                    
-                <select name="filtre-artiste" placeholder="<?php _e('Artists', 'site-theme'); ?>">
-                   
-                     <option value=""></option>
-
-                    <?php while ( $query->have_posts() ) :
-                        $query->the_post();
-
-                        /* Get only artist that have events */
-                    $program_args = array(
-                        'post_type' => array('program'),
-                        'posts_per_page' => -1,
-                        'nopaging' => true,                       
-                    );
-
-
-
-                         $program_args['meta_query'][] = array(
-                            'key' => 'artiste',
-                            'value' => serialize(strval(get_the_ID())),
-                            'compare' => 'LIKE'
-                        );
-                        $program_query = new WP_Query($program_args);
-                   
-
-                        if ($program_query->have_posts()) :
-                        
-                        $selected = false;
-                        $artist_slug = $post->post_name;
-                        if($artist_value == $artist_slug){
-                            $selected = true;
-                        }
-                        
-                        ?>
-                        <option value="<?= $artist_slug; ?>"<?= ($selected ? 'selected="selected"' : '') ?>><?php the_title(); ?></option>
-                    <?php endif; ?>     
-                    <?php endwhile; ?>
-                </select>
-            <?php endif; ?>
-
              <?php
                 $taxonomy = 'event-category';
                 $terms = get_terms($taxonomy); // Get all terms of a taxonomy
