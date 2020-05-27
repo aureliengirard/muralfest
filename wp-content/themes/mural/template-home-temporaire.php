d26408492c57f0cafe77018f03ecc8e204ee8130
Olivier Lamarche Cloutier <contact@olionline.co>
2020-05-27 17:57:55 -0400

    home temp with link to 2019

--- wp-content/themes/mural/template-home-temporaire.php
+++ wp-content/themes/mural/template-home-temporaire.php
@@ -35,7 +35,7 @@ get_header('temp'); ?>
 
 					<div class="cta link-cta">
 						<p>Plus de détails à venir. <a href="mailto:info@muralfestival.com"><span>Nous rejoindre</span></a></p>
-						<p><a href="fr/v2019"><span>cliquez ici </span></a> pour accéder au site web de 2019</p>
+						<p><a href="fr/v2019"><span>Cliquez ici </span></a> pour accéder au site web de 2019</p>
 
 					</div>
 					<div class="cta link-cta empty">
@@ -44,7 +44,7 @@ get_header('temp'); ?>
 					<div class="cta link-cta">
 						<figure>
 							<p>More details to come. <a href="mailto:info@muralfestival.com">To reach us</a></p>
-							<p><a href="/v2019"><span>cliquez ici </span></a> pour accéder au site web de 2019</p>
+							<p><a href="/v2019"><span>Click here</span></a> to access our 2019 website</p>
 						</figure>
 					</div>
 				</div>
