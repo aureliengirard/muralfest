e26fb3231444cdf510c640cea8b60990b8e8d63e
Aurelien Girard <contact@aureliengirard.com>
2022-03-16 19:44:44 -0500

    plugins update and splash page

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb030e7231ba5e4459929663950758f48::getLoader();
+return ComposerAutoloaderInitad28311747d46d5ba821077602af76bc::getLoader();
