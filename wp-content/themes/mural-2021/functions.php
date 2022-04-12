af0a8fd87ed673496b2a555fb00313f94c6a0a93
Aurelien Girard <aurelien.girard@gmail.com>
2022-04-12 16:14:05 -0500

    versionning CSS and re abilitating code for splash page redirection in function

--- wp-content/themes/mural-2021/functions.php
+++ wp-content/themes/mural-2021/functions.php
@@ -162,17 +162,17 @@ if(function_exists('acf_add_options_page')) {
 /**
  * If the redirection URL is a WordPress page or post, specify here its WordPress ID
  */
-/*
 add_action( 'template_redirect', function() {
 	$id = get_field('splash_page', 'options');
-
+	
 	if ( is_page( $id ) ) {
 		return;
 	}
-
+	
 	wp_redirect( esc_url_raw( home_url( '?page_id='.$id ) ), 307 );
 	exit;
 } );
+/*
 */
 
 /**
